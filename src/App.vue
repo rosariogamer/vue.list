a20ab2fde0880f244b027eadd7d92fbb060ad6e5
rosario gamer <82204530+rosariogamer@users.noreply.github.com>
2021-08-23 14:50:28 +0200

    hola

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="header">
+      <search v-on:query-change="querySearch" />
+    </div>
+
+    <div id="main-container">
+      <h2>Todos</h2>
+      <TodoAdd v-on:add-todo="addTodo" />
+      <Todos v-bind:todoslist="copyTodos" v-on:delete-todo="deleteTodo" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+ import Search from './components/Search';
+import Todos from "./components/Todos";
+import TodoAdd from './components/TodoAdd';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos, TodoAdd, Search
+ 
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id != id);
+      this.copyTodos = [...this.todos];
+
+
+    },
+    addTodo(todo){
+      this.todos.push(todo);
+      this.copyTodos = [...this.todos];
+    },
+    querySearch(query){
+      if(query.trim() == ''){
+        this.copyTodos = [...this.todos];
+      }else{
+        const temp = this.todos.filter(todo =>{
+          return todo.title.includes(query)
+        });
+
+        this.copyTodos = [...temp];
+
+      }
+
+    }
+
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 0,
+          title: "ir supermercado",
+          completed: false,
+        },
+        {
+          id: 1,
+          title: "comer comida sana",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "hacer yoga",
+          completed: false,
+        },
+        {
+          id: 3,
+          title: "ver serie",
+          completed: true,
+        },
+      ],
+      copyTodos: []
+    }
+  },
+  created() {
+    this.copyTodos = [...this.todos];
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.5em;
+  padding: 0;
+  margin: 0;
+}
+
+#main-container {
+  border: solid 1px #ccc;
+  width: 600px;
+  margin: 100px auto;
+}
+
+#header {
+  background: black;
+  padding: 10px;
+}
+
+h2 {
+  padding: 0 10px;
 }
 </style>
